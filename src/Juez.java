effe9ffd2b4dae3d9685056f28e53fe3b323c265
Thomas Tapia <Thomas Tapia@DESKTOP-T5JQJ45>
2021-02-20 17:55:09 -0500

    Se creo la clase juez con su variable vocalia y sus respectivos métodos get y set.

---
+++ src/Juez.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Thomas Tapia
+ */
+public class Juez {
+        
+    //Variable de instacia
+    private double vocalia;
+            
+    //Método de instancia
+
+    public double getVocalia() {
+        return vocalia;
+    }
+
+    public void setVocalia(double vocalia) {
+        this.vocalia = vocalia;
+    }
+    
+            
+}
