dbf98f3074a758ce2f06f7fbeabed0f60b28a994
Thomas Tapia <Thomas Tapia@DESKTOP-T5JQJ45>
2021-02-20 17:54:21 -0500

    Se crearon los metodos get y set para fecha, ademas se implemento el método asignacionFecha que sera modificado posteriormente dependiendo de los datos que se obtengan a travez de la GUI

---
+++ src/Partido.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Thomas Tapia
+ */
+public class Partido {
+    
+    //Variables de instancia
+    private int fecha;
+    protected Equipo numerodeEquipo;
+    
+    //Métodos de instancia
+    public String asignacionFecha(){
+        return "";
+    }
+
+    public int getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(int fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+}
